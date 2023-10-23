708d3bd28f2460ddfb52118bb95f75447ea90261
Javier Ballester <Javirb26@gmail.com>
2023-10-22 20:40:24 -0400

    completed present value ordinary annuity calculator and added comments to ordinary.js

--- scripts/ordinary.js
+++ scripts/ordinary.js
@@ -1,19 +1,34 @@
+
+
 function handleOnload() {
+    // grab the calculate button element
     const calculateOrdinaryAnnuity = document.getElementById("calculateButton");
 
+    // attach the ordinaryAnnuityOnclick function as an onclick event for the button
     calculateOrdinaryAnnuity.onclick = ordinaryAnnuityOnclick;
 }
 
+// function calculates the present value of ordinary annuity and prints the results 
 function ordinaryAnnuityOnclick() {
+    // grab input element for Payment amount, Interest rate, Years, and Present Value.
     const paymentAmountEl = document.getElementById("paymentField");
     const interestRateEl = document.getElementById("interestRateField");
     const numberOfYearsEl = document.getElementById("numberOfYearsField");
     const presentValueEl = document.getElementById("presentValueField");
 
-    const paymentAmount = paymentAmountEl.value;
-    const interestRate = interestRateEl.value / 100 / 12;
-    const years = numberOfYearsEl.value * 12;
+    // Take values from the input fields/ what is to be typed
 
+        // Payment Amount
+        const paymentAmount = paymentAmountEl.value;
+        // Interest rate converted to a monthly decimal
+        const interestRate = interestRateEl.value / 100 / 12;
+        // Years converted to months.
+        const years = numberOfYearsEl.value * 12;
+
+    // Calculate the present value using the formula for an ordinary annuity
     const presentValue = paymentAmount * (1 - (1 + interestRate) ** -years) / interestRate;
+
+    //  Update Present value input field with the calculated value rounded 2 decimals w/ toFixed(2)
     presentValueEl.value = presentValue.toFixed(2);
+    
 }
